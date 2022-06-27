84cae5373a9b8f827fff0026a3426eabbd8f0e38
Spencer A. Lockhart <s-p-n@users.noreply.github.com>
2022-06-27 16:40:41 -0600

    working on interface

--- addons/interstate/dock/dock.gd
+++ addons/interstate/dock/dock.gd
@@ -3,7 +3,7 @@ extends Control
 
 onready var url = $PanelContainer/Panel/Tree/URL/LineEdit
 onready var btn = $PanelContainer/Panel/Tree/Connect/Connect
-onready var label = $PanelContainer/Panel/Tree/Connect/Label
+onready var log_text = $PanelContainer/Panel/Tree/Connect/Panel/Log
 
 export var default_url = "localhost:4200"
 
@@ -33,26 +33,27 @@ func setup_for_connection():
 func _ready():
 	print("Dock ready")
 	url.text = default_url
+	log_text.text = "Interstate Ready."
 	setup_for_connection()
 
 func _on_fail():
-	if is_instance_valid(label):
-		label.text += "\nconnection failed."
+	if is_instance_valid(log_text):
+		log_line("Connection failed.")
 		setup_for_connection()
 
 func _on_connect(client):
-	if is_instance_valid(client) and is_instance_valid(label):
+	if is_instance_valid(client) and is_instance_valid(log_text):
 		connection = client
 		print('dock has connection: ', connection)
-		label.text += "\nConnected to %s." % url.text
+		log_line("Connected to %s." % url.text)
 		btn.text = "Disconnect"
 		btn.disabled = false
 	
 
 func _on_disconnect():
 	connection = null
-	if is_instance_valid(label):
-		label.text += "\nDisconnected from %s" % url.text
+	if is_instance_valid(log_text):
+		log_line("Disconnected from %s" % url.text)
 		btn.text = "Connect"
 		btn.disabled = false
 		url.editable = true
@@ -64,12 +65,14 @@ func _on_click():
 	if is_instance_valid(listener["obj"]) and listener["obj"].has_method(listener["method_name"]):
 		if btn.text == "Connect":
 			btn.disabled = true
-			label.text += "\nConnecting to %s.." % url.text
+			log_line("Connecting to %s.." % url.text)
 			url.editable = false
 			listener["obj"].call(listener["method_name"])
 		elif btn.text == "Disconnect" and is_instance_valid(connection):
 			connection.disconnect_from_host()
 			btn.disabled = true
-			label.text += "\nDisconnecting from %s." % url.text
+			log_line("Disconnecting from %s." % url.text)
 			
-		
+func log_line(text):
+	log_text.text += "\n%s" % text
+	log_text.cursor_set_line(log_text.get_line_count())
